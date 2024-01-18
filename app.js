2680aa37bc46674100a243989e25dca2df6cc18c
Martha <atuhairemartha62@gmail.com>
2024-01-18 10:29:01 +0300

    node

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-const sqlite3 = require('sqlite3');
+const sqlite3 = require ("sqlite3");
+const db = new sqlite3.Database('./db.sqlite');
+
+// db.run('CREATE TABLE AverageTemp (id INTEGER PRIMARY KEY, year INTEGER NOT NULL, temperature REAL NOT NULL)');
+// db.run('INSERT INTO AverageTemp(id, year, temperature) VALUES (1, 2022, 30.6)',(error)=>{
+//     if(error){
+//         console.log(error)
+//     }
+// })
+// Retriving all rows
+db.all('SELECT * FROM AverageTemp', function(error, rows){
+    if(error){
+        console.log(error)
+        return;
+    }
+    console.log(rows)
+});
+
+//Retriving single row
+db.all('SELECT * FROM AverageTemp', (error, rows)=>{
+    if(error){
+        console.log(error)
+        return;
+    }
+    console.log(rows.find(row=>row.id===1))
+
+});
