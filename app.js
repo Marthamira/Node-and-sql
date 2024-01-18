83b4e57dc0ee97bbb49b3b683cd132ce3939474a
Martha <atuhairemartha62@gmail.com>
2024-01-18 10:37:00 +0300

    db.get

--- app.js
+++ app.js
@@ -8,20 +8,29 @@ const db = new sqlite3.Database('./db.sqlite');
 //     }
 // })
 // Retriving all rows
-db.all('SELECT * FROM AverageTemp', function(error, rows){
+// db.all('SELECT * FROM AverageTemp', function(error, rows){
+//     if(error){
+//         console.log(error)
+//         return;
+//     }
+//     console.log(rows)
+// });
+
+//Retriving single row
+// db.all('SELECT * FROM AverageTemp', (error, rows)=>{
+//     if(error){
+//         console.log(error)
+//         return;
+//     }
+//     console.log(rows.find(row=>row.id===1))
+
+// });
+
+db.get('SELECT * FROM AverageTemp WHERE year=2023', (error, rows)=>{
     if(error){
         console.log(error)
         return;
     }
     console.log(rows)
-});
-
-//Retriving single row
-db.all('SELECT * FROM AverageTemp', (error, rows)=>{
-    if(error){
-        console.log(error)
-        return;
-    }
-    console.log(rows.find(row=>row.id===1))
 
 });
